1a778cf83ca08b4ebf864befd04b30770a3f3326
festere <104324449+festere@users.noreply.github.com>
2023-03-23 20:20:03 +0100

    Add files via upload

--- launcher.cmd
+++ launcher.cmd
@@ -1,12 +1,25 @@
 @echo off
 echo Veillez a bien prendre en compte les informations suivantes :
-echo - Un point de restoration sera cree sous le nom de "RestorePointBeforeHardening".
+echo - Un point de restoration sera cree sous le nom de [101m"RestorePointBeforeHardening"[0m.
+echo[
+
 echo - Le programme va modifier les permissions/parametre/autorisation/... de certains fichiers/dossiers/registres/services/... de votre ordinateur (plus d'informations dans le fichier "README.md")
+echo[
+
 echo - Le programme peut afficher des messages d'erreurs, ne pas les prendre en compte (sauf si le programme s'arrete).
-echo - Le programme peut prendre du temps a s'executer, ne pas fermer le programme avant la fin TOTALE de l'execution.
-echo - Ne JAMAIS relancer le programme memre si il s'est arrete, cela peut entrainer des problemes (utiliser le point de restauration cree au debut du programme).
-echo - Fermez TOUS les programmes avant de lancer le programme.
-echo - Ne JAMAIS cliquer sur aucun bouton pendant l'execution du programme meme si propose (toutes les actions sont automatiques).
+echo[
+
+echo - Le programme peut prendre du temps a s'executer, ne pas fermer le programme avant la fin [101mTOTALE[0m de l'execution.
+echo[
+
+echo - Ne [101mJAMAIS[0m relancer le programme memre si il s'est arrete, cela peut entrainer des problemes (utiliser le point de restauration cree au debut du programme).
+echo[
+
+echo - Fermez [101mTOUS[0m les programmes avant de lancer le programme.
+echo[
+    
+echo - Ne [101mJAMAIS[0m cliquer sur aucun bouton pendant l'execution du programme meme si propose (toutes les actions sont automatiques).
+
 
 @echo off
 REM  :: Analyse les permissions
